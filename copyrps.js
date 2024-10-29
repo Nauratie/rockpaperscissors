97c5142606e732270c2db88e6b50c8a9e3b3f496
Nauratie <kohl.michael92@gmail.com>
2024-10-29 14:09:27 +0100

    add new file(copy)

---
+++ copyrps.js
@@ -0,0 +1,57 @@
+let randomNumber = Math.random();
+
+function getComputerChoice() {
+  
+  if(randomNumber<0.3333) {
+  return "Scissors"; 
+  } else if(randomNumber>0.6666) {
+  return "Rock"; 
+  } else {
+  return "Paper"; 
+  }
+}
+
+console.log(getComputerChoice(randomNumber));
+
+let humanChoice = prompt("Choose Rock, Paper or Scissors", "Rock");
+
+function getHumanChoice() {
+
+  if(humanChoice=="Rock") {
+  return "Rock";
+  } else if(humanChoice=="Scissors") {
+  return "Scissors"; 
+  } else if (humanChoice=="Paper") {
+  return "Paper"; 
+  } else {
+  alert("Insert either Rock, Paper or Scissors");
+  }
+}
+
+console.log(getHumanChoice());
+
+let humanScore = 0
+let computerScore = 0
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound() {
+
+  if(humanSelection===computerSelection) {
+  alert("Tie");
+  } else if(humanSelection==="Rock" && computerSelection==="Scissors") {
+  return ++humanScore && alert("You win!");
+  } else if(humanSelection==="Scissors" && computerSelection==="Paper") {
+  return ++humanScore && alert("You win!");
+  } else if(humanSelection==="Paper" && computerSelection==="Rock")  {
+  return ++humanScore && alert("You win!");
+  } else {
+  return ++computerScore && alert("You lose.");
+  }
+}	 
+
+playRound();
+
+console.log(humanScore);
+console.log(computerScore);
