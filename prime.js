916e9e6a104c32a45f78a46fa5f20e59011fdab3
Nauratie <kohl.michael92@gmail.com>
2024-11-07 11:13:21 +0100

    add loop

--- prime.js
+++ prime.js
@@ -2,13 +2,16 @@ let n = prompt("Put a number:");
 let prime = 2; 
 
 function isPrime(n) {
-  for (let i = 2; i < n; i++) {
-    if (n%i == 0) {
-      continue;
+  for (let i = 3; i < n; i++) {
+    for (let j = 2; j < i; j++)
+      if (i%j == 0) {
+      break;
     } else {
-      let prime = prime +=`, ${n}`;
+      let prime = prime +=`, ${i}`;
     }
   }
 
 alert(prime);
 }
+
+isPrime(n);
