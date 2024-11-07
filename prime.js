2bef64c1be8ce8e537e1018524a75f224c9073b1
Nauratie <kohl.michael92@gmail.com>
2024-11-07 10:41:51 +0100

    add semicolons

--- prime.js
+++ prime.js
@@ -1,12 +1,12 @@
 let n = prompt("Put a number:");
-let prime = 2 
+let prime = 2; 
 
 function isPrime(n) {
   for (let i = 2; i < n; i++) {
     if (n%i == 0) {
       continue;
     } else {
-      let prime += n
+      let prime += n;
     }
   }
 
