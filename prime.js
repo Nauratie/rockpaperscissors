bcaa5d882b3e1594d1604ae4c334cbb2ad2be5b9
Nauratie <kohl.michael92@gmail.com>
2024-11-07 11:50:10 +0100

    delete else clause and function

--- prime.js
+++ prime.js
@@ -1,17 +1,9 @@
 let n = prompt("Put a number:");
-let prime = 2; 
 
-function isPrime(n) {
-  for (let i = 3; i < n; i++) {
+  outer: for (let i = 3; i < n; i++) {
     for (let j = 2; j < i; j++)
       if (i%j == 0) {
-      break;
-    } else {
-      let prime = prime +=`, ${i}`;
-    }
+      continue outer;
+      }
+    alert(i);    
   }
-
-alert(prime);
-}
-
-isPrime(n);
