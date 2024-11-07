ed9f8441d14265f4ac6a6c20ea0cf305b568aa43
Nauratie <kohl.michael92@gmail.com>
2024-11-07 10:24:00 +0100

    create new file, add function

---
+++ prime.js
@@ -0,0 +1,14 @@
+let n = prompt("Put a number:");
+let prime = 2 
+
+funtion isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if (n%i = 0) {
+      continue;
+    } else {
+      let prime += n
+    }
+  }
+
+alert(prime);
+}
